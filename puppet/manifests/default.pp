2a0bdabba2683f91efbd7183458c33dc8b593db5
Karl Podger <karl@wednesdayagency.com>
2014-01-07 01:46:40 +0000

    Set correct Puppet 3.4 syntax to work with latest version of Vagrant.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -295,7 +295,7 @@ if count($php_values['modules']['pecl']) > 0 {
   php_pecl_mod { $php_values['modules']['pecl']:; }
 }
 if count($php_values['ini']) > 0 {
-  each( $php_values['ini'] ) { |$key, $value|
+  each( $php_values['ini'] ) |$key, $value| {
     puphpet::ini { $key:
       entry       => "CUSTOM/${key}",
       value       => $value,
@@ -366,7 +366,7 @@ if $xdebug_values['install'] != undef and $xdebug_values['install'] == 1 {
   }
 
   if is_hash($xdebug_values['settings']) and count($xdebug_values['settings']) > 0 {
-    each( $xdebug_values['settings'] ) { |$key, $value|
+    each( $xdebug_values['settings'] ) |$key, $value| {
       puphpet::ini { $key:
         entry       => "XDEBUG/${key}",
         value       => $value,
